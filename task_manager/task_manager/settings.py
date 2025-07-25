2239ba2b02f199eee42beead769f5e90161455d6
ashi <ashfaq319535@gmail.com>
2025-07-25 16:57:40 +0500

    solve the .env file issue in setting.py for tests

--- task_manager/task_manager/settings.py
+++ task_manager/task_manager/settings.py
@@ -208,13 +208,13 @@ SPECTACULAR_SETTINGS = {
     'VERSION': '1.0.0',
 }
 
-# Email configuration (from .env)
+# Email configuration
 EMAIL_BACKEND = env('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
 EMAIL_HOST = env('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = env.int('EMAIL_PORT', default=587)
 EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=True)
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='test@example.com')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='testpassword')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Celery configuration (from .env)
